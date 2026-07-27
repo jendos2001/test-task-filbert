a5aedc842f9ba87a20a50f178a035107662edfd6
jendos2001 <Jendos2001@yandex.ru>
2026-07-27 16:08:47 +0300

    task4: Done

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from random import sample
+from random import sample, randint
 from time import sleep
 
 
@@ -13,6 +13,10 @@ login = 'standard_user'
 password = 'secret_sauce'
 
 options = Options()
+prefs = {
+        "profile.password_manager_leak_detection": False
+}
+options.add_experimental_option("prefs", prefs)
 #options.add_argument('--headless=new')
 
 driver = webdriver.Chrome(options=options)
@@ -26,20 +30,14 @@ driver.find_element(By.ID, 'login-button').click()
 #!--------task2-3--------!
 WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'root')))
 
-try:
-    alert_obj = driver.switch_to.alert 
-    alert_obj.accept()
-
-except:
-    pass
-
 items_count = 3
 items = driver.find_element(By.CLASS_NAME, 'inventory_list').find_elements(By.CLASS_NAME, 'inventory_item')
-buy_items = sorted(sample(range(len(items)), items_count))
+buy_items_indecies = sorted(sample(range(len(items)), items_count))
 k = 0
+
 database = []
 for index, item in enumerate(items):
-    if index == buy_items[k]:
+    if index == buy_items_indecies[k]:
         title = item.find_element(By.CLASS_NAME, 'inventory_item_name').text 
         description = item.find_element(By.CLASS_NAME, 'inventory_item_desc').text 
         price = float(item.find_element(By.CLASS_NAME, 'inventory_item_price').text[1:])
@@ -48,6 +46,10 @@ for index, item in enumerate(items):
         k += 1
         if k == items_count:
             break
+
+#!--------task4--------!
+delete_item_index = randint(0, items_count - 1)
+delete_item = database.pop(delete_item_index)
 
 sleep(30)
 
