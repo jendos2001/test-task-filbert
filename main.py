aadb4a9cd0859829d198f77ca16dc3c7d7190d78
jendos2001 <Jendos2001@yandex.ru>
2026-07-27 16:57:58 +0300

    task10:Done

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from random import sample, randint
 from time import sleep
+import os
 
 
 
 site_url = 'https://www.saucedemo.com/'
 login = 'standard_user'
 password = 'secret_sauce'
+download_dir = os.path.abspath(os.getcwd()) + '/Downloads'
 
 options = Options()
 prefs = {
-        "profile.password_manager_leak_detection": False
+        "profile.password_manager_leak_detection": False,
+        "download.default_directory": download_dir,
+        "safebrowsing.enabled": True,
 }
 options.add_experimental_option("prefs", prefs)
 #options.add_argument('--headless=new')
@@ -50,7 +54,6 @@ for index, item in enumerate(items):
 #!--------task4--------!
 delete_item_index = randint(0, items_count - 1)
 delete_item = database.pop(delete_item_index)
-print(delete_item[0])
 
 #!--------task5--------!
 driver.find_element(By.CLASS_NAME, 'shopping_cart_link').click()
@@ -85,6 +88,11 @@ shipping_information = info_items[1].text
 tax = float(driver.find_element(By.CLASS_NAME, 'summary_tax_label').text.split()[1][1:])
 total = float(driver.find_element(By.CLASS_NAME, 'summary_total_label').text.split()[1][1:])
 info_item_block.find_element(By.ID, 'finish').click()
+
+#!--------task10--------!
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'root')))
+driver.find_element(By.ID, 'generate-pdf-order').click()
+
 
 sleep(30)
 
