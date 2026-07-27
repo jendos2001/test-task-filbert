5b41babcbb8e270bb2c4b034a0de4f01316647a6
jendos2001 <Jendos2001@yandex.ru>
2026-07-27 15:53:27 +0300

    task2-3: Done

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from random import sample
 from time import sleep
 
 
@@ -17,10 +18,36 @@ options = Options()
 driver = webdriver.Chrome(options=options)
 driver.get(site_url)
 
-#!--------task1-----------!
+#!--------task1--------!
 driver.find_element(By.ID, 'user-name').send_keys(login)
 driver.find_element(By.ID, 'password').send_keys(password)
 driver.find_element(By.ID, 'login-button').click()
-sleep(5)
 
-#print(main_page)
+#!--------task2-3--------!
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'root')))
+
+try:
+    alert_obj = driver.switch_to.alert 
+    alert_obj.accept()
+
+except:
+    pass
+
+items_count = 3
+items = driver.find_element(By.CLASS_NAME, 'inventory_list').find_elements(By.CLASS_NAME, 'inventory_item')
+buy_items = sorted(sample(range(len(items)), items_count))
+k = 0
+database = []
+for index, item in enumerate(items):
+    if index == buy_items[k]:
+        title = item.find_element(By.CLASS_NAME, 'inventory_item_name').text 
+        description = item.find_element(By.CLASS_NAME, 'inventory_item_desc').text 
+        price = float(item.find_element(By.CLASS_NAME, 'inventory_item_price').text[1:])
+        item.find_element(By.CLASS_NAME, 'btn').click()
+        database.append((title, description, price))
+        k += 1
+        if k == items_count:
+            break
+
+sleep(30)
+
