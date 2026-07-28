c963ef8e39354d670d3b6fd55cf8c56ddc29beaf
jendos2001 <Jendos2001@yandex.ru>
2026-07-28 14:17:40 +0300

    create worker class

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from parcer import parsers
+from parcer.parsers import Worker
+
+site_url = 'https://www.saucedemo.com/'
+login = 'standard_user'
+password = 'secret_sauce'
+download_dir = '/Downloads'
+items_count = 3
+first_name = 'Evgeny'
+last_name = 'Kurshev'
+postal_code = '187550'
+
 
 if __name__ == '__main__':
-    parsers.main()
+    worker = Worker(site_url, login, password, download_dir, 
+                    items_count, first_name, last_name, postal_code)
+    worker.start()
 
